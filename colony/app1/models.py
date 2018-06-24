8af95d4b1ccbbf892a407823ee4509a76f0cbbbf
Aviraj Khare <avirajkhare00@gmail.com>
2018-06-24 19:57:21 +0530

    models created

--- colony/app1/models.py
+++ colony/app1/models.py
@@ -5,3 +5,24 @@ from django.db import models
 
 # Create your models here.
 
+class CreateProject(models.Model):
+
+    project_title = models.CharField(max_length=100, null=True, blank=True)
+    domainId = models.CharField(max_length=5, null=True, blank=True)
+    project_brief = models.TextField(null=True, blank=True)
+    creator_address = models.CharField(max_length=100, null=True, blank=True)
+    creator_name = models.CharField(max_length=100, null=True, blank=True)
+
+    def __unicode__(self):
+
+        return self.project_title
+
+
+class ProjectTag(models.Model):
+
+    tag = models.CharField(max_length=100, null=True, blank=True)
+    tag_body = models.TextField(null=True, blank=True)
+
+    def __unicode__(self):
+
+        return self.tag
