b46d3753fd90e7be8957c5a851cbee3f21a6dcb4
Aviraj Khare <avirajkhare00@gmail.com>
2018-06-24 20:43:52 +0000

    added many files

--- colony/app1/admin.py
+++ colony/app1/admin.py
@@ -2,5 +2,8 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-
+from models import CreateProject, ProjectTag
 # Register your models here.
+
+admin.site.register(CreateProject)
+admin.site.register(ProjectTag)
