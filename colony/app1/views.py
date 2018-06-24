5ce9cf21a2a9f41d8b67776dcfc95189dc2118e3
Aviraj Khare <avirajkhare00@gmail.com>
2018-06-24 18:52:33 +0530

    index view added

--- colony/app1/views.py
+++ colony/app1/views.py
@@ -1,6 +1,11 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from django.shortcuts import render, redirect
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.contrib.auth import login, authenticate, logout
 
-from django.shortcuts import render
 
-# Create your views here.
+class IndexView(APIView):
+
+    def get(self, request):
+
+        return render(request, 'html/index.html')
--- colony/colony/urls.py
+++ colony/colony/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from app1.views import IndexView
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', IndexView.as_view(), name='index_page'),
 ]
